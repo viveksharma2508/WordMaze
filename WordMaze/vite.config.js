f6b20922419e227842679cd578b654e4f6c336d2
viveksharma2525 <sharma.vivek0910@outlook.com>
2024-09-12 19:21:47 +0530

    final

--- WordMaze/vite.config.js
+++ WordMaze/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
-});
+})
